32394fa7efb643446fd30d2a8966bc83abb34d22
TARGINO0110 <lucianoptargino1@outlook.com>
2020-02-28 14:01:21 -0300

    Atualizando unidade 26/27/28/29/30 apostila 2

---
+++ resources/views/apostila2/unidade29/pronunciation.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.template')
+@section('titulo','UNIMUGI | Home')
+@section('conteudo')
+<!-- WRAPPER ALL -->
+
+	@include('layouts.menus.mSidebar')
+    @include('layouts.header.mHeader')
+    		
+	<main>
+		<!-- Conteúdo principal central -->
+		<div class="dashboard">
+			<div id="unidade" class="box" data-apostila="apostila2" data-unidade="unidade29" data-etapa="pronunciation">
+				<h3 class="barlow">UNIT 29</h3>
+				<h5 class="barlow">4 - PRONUNCIATION</h5>
+				<span class="play-audio">( Aperte o play )</span>
+				<audio controls="" controlslist="nodownload" class="suave">
+					<source src="{{ asset('assets/audio29/Pronunciation/completo.ogg') }}" type="audio/ogg">
+                </audio>
+				<div class="clear"></div>
+				<ul class="lista-inline">
+					<li><b>ch sound /ʧ/</b></li>        
+					<li>Much</li>	
+					<li>Teach</li>
+					<li>Watch</li>
+					<li>Challenge</li>
+					<li>Change</li>
+					<li>Channel</li>
+					<li>Choose</li>
+					<li>Check</li>
+				</ul>
+				<ul class="lista-inline">
+					<ol>The tea<b>ch</b>er is going to <b>ch</b>ange the <b>cha</b>llenge to <b>ch</b>eck how mu<b>ch</b> time students spend <b>choo</b>sing and wat<b>ch</b>ing youtube <b>cha</b>nnels.</ol>
+				</ul>
+				<div class="clear"></div>
+			</div>
+		</div>
+    </main>
+    <script>
+        activeMenu();
+    </script>
+
+@endsection
