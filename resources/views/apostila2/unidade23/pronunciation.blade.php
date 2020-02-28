0dc9bc53b7660f5c73ae6d805cee13255cffa34c
williame <williame_lima@hotmail.com>
2020-02-28 11:48:51 -0300

    apostila02_unidade23_concluida

---
+++ resources/views/apostila2/unidade23/pronunciation.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.template')
+@section('titulo','UNIMUGI | Home')
+@section('conteudo')
+<!-- WRAPPER ALL -->
+
+	@include('layouts.menus.mSidebar')
+    @include('layouts.header.mHeader')
+    		
+	<main>
+		<!-- Conteúdo principal central -->
+		<div class="dashboard">
+			<div id="unidade" class="box" data-apostila="apostila2" data-unidade="unidade23" data-etapa="pronunciation">
+				<h3 class="barlow">UNIT 23</h3>
+				<h5 class="barlow">4 - PRONUNCIATION</h5>
+				<span class="play-audio">( Aperte o play )</span>
+				<audio controls="" controlslist="nodownload" class="suave">
+					<source src="{{ asset('assets/audio/unit1/pronuciation/pronuciation.ogg') }}" type="audio/ogg">
+                </audio>
+                <div class="clear"></div>
+                <p><b>schwa+r /ɚ/</b></p>
+                
+                <ul class="lista-inline">
+                    <li><p>HER</p></li>
+                    <li><p>WORK</p></li>
+                    <li><p>SURE</p></li>
+                    <li><p>FIRST</p></li>
+                    <li><p>EARLY</p></li>
+                    <li><p>WERE</p></li>
+                </ul>
+
+                <p>Fi<b>r</b>st, they w<b>e</b>re, for s<b>u</b>re, in h<b>e</b>r w<b>o</b>rk very <b>ea</b>rly.</p>
+
+			</div>
+		</div>
+    </main>
+    <script>
+        activeMenu();
+    </script>
+
+@endsection
