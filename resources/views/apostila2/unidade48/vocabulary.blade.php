5b9051d488fec319cefc7a5fbbc8e02f2aab89ce
williame <williame_lima@hotmail.com>
2020-03-04 09:41:31 -0300

    apostila02_unidade48_concluida

---
+++ resources/views/apostila2/unidade48/vocabulary.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.template')
+@section('titulo','UNIMUGI | Home')
+@section('conteudo')
+<!-- WRAPPER ALL -->
+
+	@include('layouts.menus.mSidebar')
+    @include('layouts.header.mHeader')
+    		
+	<main>
+		<!-- Conteúdo principal central -->
+		<div class="dashboard">
+			<div id="unidade" class="box" data-apostila="apostila2" data-unidade="unidade48"  data-etapa="vocabulary">
+
+				<h3 class="barlow">UNIT 48</h3>
+				<h5 class="barlow">1 - VOCABULARY</h5>
+				<span class="play-audio">( Aperte o play )</span>
+				<audio controls="" controlslist="nodownload" class="suave">
+					<source src="{{ asset('assets/audio/unit1/vocabulary/vocabulary.ogg') }}" type="audio/ogg">
+				</audio>
+				<h5 class="barlow" style="margin-top: 16px">A - WORDS AND EXPRESSIONS</h5>
+
+				<p>TO PRETEND TO BE / happy / sad / sorry / another person</p>
+				<p>TO SAY / a few words / something / nothing</p>
+				<p>TO SURF / at the beach / the net</p>
+
+				<h5 class="barlow" style="margin-top: 16px">B - WORDS IN CONTEXT</h5>
+
+				<ul class="lista-inline">
+					<li><b>SMART</b></li>
+					<li><b>TRY</b></li>
+					<li><b>LIKE</b></li>
+					<li><b>BUTTERFLY</b></li>
+					<li><b>VIEW</b></li>
+					<li><b>CAPTIVATE</b></li>
+					<li><b>SAW</b></li>
+					<li><b>THINK</li>
+				</ul>							
+				
+			</div>
+		</div>
+	</main>
+	<script>
+        activeMenu();
+    </script>
+
+@endsection
