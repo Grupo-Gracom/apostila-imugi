a81bc7523fb4560889c898f8239352003e2de417
TARGINO0110 <lucianoptargino1@gmail.com>
2020-04-06 12:24:49 -0300

    Adicionando unidade 8 da apostila 4

--- resources/views/layouts/menus/mSidebar.blade.php
+++ resources/views/layouts/menus/mSidebar.blade.php
@@ -2120,6 +2120,22 @@
                         </li>
                     </ul>
                 </li>
+                <!--UNIDADE 8-->
+                <li class="item" data-unidade="unidade8">
+                    <a class="click submenu">
+                        <i class="material-icons">flag</i> UNIT 8
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="{{route('apostila4', ['unidade8', 'vocabulary'])}}" data-etapa="vocabulary"><i class="material-icons suave">record_voice_over</i> Vocabulary</a>
+                            <a href="{{route('apostila4', ['unidade8', 'conversation'])}}" data-etapa="conversation"><i class="material-icons suave">forum</i> Conversation</a>
+                            <a href="{{route('apostila4', ['unidade8', 'grammar'])}}" data-etapa="grammar"><i class="material-icons suave">edit</i> Grammar</a>
+                            <a href="{{route('apostila4', ['unidade8', 'pronunciation'])}}" data-etapa="pronunciation"><i class="material-icons suave">record_voice_over</i> Pronunciation</a>
+                            <a href="{{route('apostila4', ['unidade8', 'listening'])}}" data-etapa="listening"><i class="material-icons suave">hearing</i> Listening</a>
+                            <a href="{{route('apostila4', ['unidade8', 'understanding'])}}" data-etapa="understanding"><i class="material-icons suave">local_library</i> Understanding</a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </li>
         <!-- APOSTILA GAMES AND ANIMATION -->
