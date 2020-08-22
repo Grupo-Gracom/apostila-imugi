8333c01ec20abbc54b24e4bc0023a95978ac801a
luisjonson <luizlindonjonson@yahoo.com.br>
2020-08-22 00:00:03 -0300

    aula 8 da apostila 6 do capitulo 3

--- resources/views/apostila6/unidade3/atividades_tiposde_de_corte.blade.php
+++ resources/views/apostila6/unidade3/atividades_tiposde_de_corte.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.template')
-@section('titulo','Unit 2 | Atividades')
+@section('titulo','Unit 3 | atividades Tiposde de corte')
 @section('conteudo')
 <!-- WRAPPER ALL -->
 
@@ -9,14 +9,14 @@
 <main>
 	<!-- Conteúdo principal central -->
 	<div class="dashboard">
-		<div id="unidade" class="box" data-apostila="apostila5" data-unidade="unidade2" data-etapa="atividades">
+		<div id="unidade" class="box" data-apostila="apostila6" data-unidade="unidade3" data-etapa="atividades_tiposde_de_corte">
 			<h3 class="barlow">UNIT 3</h3>
-			<h5 class="barlow upper">Aula 7 – Atividades</h5>
+			<h5 class="barlow upper">Aula 7 – Atividades - Tipos de corte</h5>
                 <form method="post" class="metade">
 					<p>1 - Criar transições entre cenas de videos capturados com a camera.
 						<input type="text" class="full left-align" placeholder="Atividade Prática" disabled="true">
 					</p>
-					<p>2- Aplicar tecnicas de ti pos de corte apresentados em sala usando tecnicas de edição.
+					<p>2 - Aplicar tecnicas de ti pos de corte apresentados em sala usando tecnicas de edição.
 						<input type="text" class="full left-align" placeholder="Atividade Prática" disabled="true"> 
 					</p>
 					
