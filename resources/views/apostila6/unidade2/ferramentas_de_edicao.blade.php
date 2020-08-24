6d11ecd52c1f21252159fa889d0c7f80fc43433f
yurifa <yurialcantara03@gmail.com>
2020-08-24 15:43:49 -0300

    finalizando aula 5

--- resources/views/apostila6/unidade2/ferramentas_de_edicao.blade.php
+++ resources/views/apostila6/unidade2/ferramentas_de_edicao.blade.php
@@ -270,401 +270,96 @@
 			</p>
 
 
-			<h5>Regra dos Terços</h5>
+			<h5>Slip Edit e Slide Edit</h5>
 
 			<p>
-				Para um estudo aprofundado e uma exatidão maior no enquadramento, ao olharmos pelo visor da 
-				câmera podemos dividir mentalmente o quadro em três partes (três partes horizontais
-				e três partes verticais).
+				Assim como as edições ripple e rolling elas permitem que você ajuste um corte entre
+				dois clipes, as edições slip e slide são úteis quando você deseja ajustar dois cortes em
+				uma seqüência de três clipes. Quando você usas ferramentas slip ou slide, o Program
+				Monitor exibe os quatro quadros envolvidos na edição lado a lado, exceto quando se
+				edita apenas áudio.
 			</p>
+
+			<h5>
+				Slip Edit
+				<img style="display: inline-block;" src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_37.png') }}" alt="guia"> 
+			</h5>
 
 			<div class="container">
 				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_1.png') }}" alt="guia1">
+				<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_38.png') }}" alt="guia1">
 				</figure>
 			</div>
 
 			<p>
-				Os melhores e mais bem equilibrados enquadramentos são aqueles em que os assuntos
-				principais que se pretende filmar ou exibir estão em um dos quatro pontos de interseção
-				na divisão simples da Regra dos Terços, e não no quadrado do meio.
-			</p>
-
-			<p>
-				Por exemplo, os olhos de um personagem devem ficar na linha superior. O horizonte não deve
-				ficar no centro do quadro e sim na linha superior ou na inferior quando se quiser dar mais
-				ênfase ao primeiro plano. Esta é uma regra que deve ser seguida em tomadas normais, mas
-				pode-se por razões dramáticas ou para isolar um objeto do todo, enquadrar de outra maneira.
+				Uma edição slip muda os pontos de entrada e saída de um clip para frente ou para trás pelo
+				mesmo número de quadros em uma única ação. Ao arrastar com a ferramenta Slip, você pode
+				alterar os quadros iniciais e fi nais de um clip sem alterar a duração ou afetar clips adjacentes.
 			</p>
 
 			<div class="container">
 				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_2.png') }}" alt="guia1">
+				<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_39.png') }}" alt="guia1">
 				</figure>
 			</div>
 
-			<h5>Grande plano geral</h5>
+			<h5>
+				Slide Edit
+				<img style="display: inline-block;" src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_40.png') }}" alt="guia"> 
+			</h5>
+
+			<div class="container">
+				<figure>
+				<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_41.png') }}" alt="guia1">
+				</figure>
+			</div>
 
 			<p>
-				O plano mais aberto ou abrangente é o grande plano geral. Sua principal característica vem de
-				sua função: passar ao espectador referência geográfica. As imagens de uma praia, montanha
-				ou deserto, captadas em longas distâncias são bons exemplos de GPG. É importante destacar
-				que, pela distância da câmera para o objeto, é impossível se reconhecer uma pessoa, mas permite 
-				reconhecer a existência de uma multidão de pessoas. Geralmente, é utilizado no começo
-				de uma sequência para passar ao espectador a referência do local onde acontece a ação.
-			</p>
-
-			<p>
-				Em filmes onde a trama se desenrola em diferentes locais do mundo o GPG é praticamente
-				indispensável.
+				Uma edição de slide muda um clipe no tempo ao cortar clips adjacentes para compensar
+				o movimento. À medida que você arrastou um clip para a esquerda ou para a direita com
+				a ferramenta Slide, o ponto de saída do clip anterior e o ponto In do seguinte clip são cortados 
+				pelo número de quadros que você move o clip. Os pontos de entrada e saída do clip
+				(e, portanto, a duração) permanecem inalterados.
 			</p>
 
 			<div class="container">
 				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_3.png') }}" alt="guia1">
+					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_42.png') }}" alt="guia1">
 				</figure>
 			</div>
 
-			<h5>O plano geral</h>
+			<h5>Adobe Media Encoder</h5>
 
 			<p>
-				Com a função de passar uma referência mais específica do local, o plano geral já permite
-				identificar pessoas. A fachada de um prédio, a Casa Branca, um carro estacionando em uma
-				garagem, todos estes são bons exemplos de PG.
+				Premiere Pro e outras aplicações empregam o Adobe Media Encoder; um aplicati vo de codificação 
+				autônomo. Quando você especifi ca confi gurações de exportação na caixa de diálogo
+				Export Setti ngs e clica em Queue, o Premiere Pro envia a solicitação de exportação para o
+				Adobe Media Encoder.
+			</p>
+
+
+			<div class="container">
+				<figure>
+					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_43.png') }}" alt="guia1">
+				</figure>
+			</div>
+
+			<p>
+				Quando o Adobe Media Encoder está sendo processado e exportado em segundo plano, você
+				pode conti nuar a trabalhar no Premiere Pro. Adobe Media Encoder codifi ca a versão mais recente 
+				de cada sequência na fi la.
 			</p>
 
 			<div class="container">
 				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_4.png') }}" alt="guia1">
+					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_44.png') }}" alt="guia1">
 				</figure>
 			</div>
-
-			<h5>Planos de relação Humanóide</h5>
-
-			<p>
-				Esses planos fazer relação com o corpo humano, onde fazem referencia com partes do corpo
-				e somente são válidos para seres humanos.
-			</p>
-
-			<h5>Plano Americano</h5>
-
-			<p>
-				É o plano que enquadra a figura humana do joelho para cima. Geralmente não comporta mais
-				do que três personagens reunidas. Tem esse nome devido à sua grande popularidade entre os
-				diretores de Hollywood das décadas de 30 e 40.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_5.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<h5>Plano conjunto</h5>
-
-			<p>
-				Forma um pequeno grupo de pessoas ou ambiente determinado. Interessa a ação e a situação
-				das pessoas. Normalmente teremos valores descritivos ou narrativos.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_6.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<h5>Plano médio</h5>
-
-			<p>
-				A câmera está a uma distância média do objeto, de modo que ele ocupa uma parte considerável do ambiente, 
-				mas ainda tem espaço à sua volta.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_7.png') }}" alt="guia1">
-				</figure>
-			</div>
-			
-			<h5>Plano fechado</h5>
-
-			<p>
-				a câmera está bem próxima do objeto, de modo que ele ocupa quase todo o cenário, sem
-				deixar grandes espaços à sua volta. É um plano de INTIMIDADE e EXPRESSÃO.
-			</p>
-			
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_8.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<h5>Plano close</b>
-
-			<p>
-				A figura humana é enquadrada dos ombros para cima. Também chamado de “BIG CLOSE-UP”
-				ou “BIG-CLOSE”. Também é um plano que expressa sentimentos, por esta razão é muito utilizado 
-				em teledramaturgia e em filmes românticos.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_9.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<h5>Plano detalhe</b>
-
-			<p>
-				O detalhe muitas vezes é fundamental para a condução da narrativa visual. Ele é o responsável
-				por tornar evidente este ou aquele objeto em cena. É neste momento também que o espectador 
-				está à mercê do produtor de vídeo, que mostra os detalhes daquilo que lhe convém e
-				oculta qualquer traço de informação que possa causar uma impressão diferente daquilo que
-				é pretendido.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_10.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<h5>Plongeé Absoluto</h5>
-
-			<p>
-				A câmara é colocada no alto do cenário, apontando diretamente para baixo.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_11.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<p>
-				Neste ângulo a câmera está posicionada acima do objeto filmado, em um ângulo superior,
-				enquadrando de cima para baixo e dando a impressão de achatamento ou inferioridade.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_12.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<h5>Contra-plongée</h5>
-
-			<p>
-				A câmera colocada abaixo do objeto faz com que o espectador veja a cena de baixo para
-				cima (por exemplo, abaixo do nível do olhar do personagem). Ao contrário da câmera alta
-				dá a impressão de superioridade.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_13.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<h5>Contra-plongée Absoluto</h5>
-
-			<p>
-				A câmera aponta diretamente para cima.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_14.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<h5>Câmera na Diagonal</h5>
-
-			<p>
-				Gera um desequilíbrio na imagem criando uma tensão interna. É usada para revelar estados 
-				de desequilíbrio. Por exemplo: pessoa passando mal — câmera em close-up pegando
-				o rosto de lado.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_15.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<h5>Câmera subjetiva</h5>
-
-			<p>
-				É quando o espectador ou o ator tem o ponto de vista da câmera ou se move no lugar dela.
-				Muito utilizada em cenas de deslocamento do ator, em que a câmera na mão do operador
-				assume o ponto de vista do ator em movimento.
-			</p>
-
-			<div class="container">
-				<figure>
-					<img src="{{ asset('assets/img/video_edition/unit2/imagem_unit2_16.png') }}" alt="guia1">
-				</figure>
-			</div>
-
-			<h5>Movimentos de camera</h5>
-
-			<p>
-				No início da história do cinema, a câmera ficava estática capturando as imagens. Somente as
-				pessoas e objetos se moviam diante dela. Cabe lembrar que os equipamentos eram desajeitados 
-				e pesados. Em pouco tempo, a câmera passou a captar imagens em movimento de dentro
-				de alguns veículos como barcos e trens. surgiam o travelling e os movimentos de aproximação
-				e afastamento das cenas. A câmera de cinema ganha então a capacidade de interagir com a
-				cena e explorar uma linguagem que era impossível na fotografia estática.
-			</p>
-
-			<h5>Travelling</h5>
-
-			<p>
-				O travelling surgiu com o deslocamento lateral da câmera (da direita para a esquerda e
-				vice-versa), colocando-a junto à janela de passageiro em um veículo em movimento.
-			</p>
-			<p>
-				A aproximação e o afastamento das imagens utilizavam o mesmo processo, mas posicionando
-				a câmera na frente ou atrás do veículo. Em todas as situações, a sensação de velocidade era
-				surpreendente. Nos EUA, durante os anos 10, se popularizou um tipo de cinema que imitava
-				vagões de trem e utilizavam filmagens que simulavam viagens pelo país.
-			</p>
-			<p>
-				Hoje o deslocamento lateral da câmera pode ser feito de muitas formas devido ao avanço dos
-				equipamentos de filmagem.
-			</p>
-
-			<h5>Os movimentos panorâmicos e a base articulada do tripé</h5>
-
-			<p>
-				O próximo grande passo foia criação de uma base articulada que se fixa entre o tripé de sustentação 
-				e a câmera. A idéia não era muito inovadora, os equipamentos medição à distância
-				da construção civil já empregavam este equipamento, no entanto a cabeça articulada permitia
-				a fácil mobilidade dos movimentos, sem que a câmera saísse do local.
-			</p>
-
-			<h5>A panorâmica horizontal</h5>
-
-			<p>
-				O deslocamento lateral da câmera em torno do próprio eixo permite a ampliação do campo
-				visual. Pode parecer, à primeira vista, igual ao travelling, mas possui profundas diferenças.
-				Primeiro, a pan-horizontal depende apenas do deslocamento em torno do próprio eixo, enquanto 
-				o travelling depende do deslocamento do local através de um equipamento específico.
-				A segunda e maior diferença está na dimensão espacial da imagem. Enquanto o travelling expõe 
-				uma parte do ambiente que vai se revelado em partes, a panorâmica horizontal permite,
-				em um único e breve movimento, revelar toda a dimensão da imagem.
-			</p>
-			
-			<h5>Panorâmica vertical</h5>
-
-			<p>
-				A panorâmica vertical é muito utilizada para destacar a altura de algo, geralmente, deixar em
-				evidência a altura de um grande prédio, monumento ou captar a imagem de algo que se desloca 
-				verticalmente como um elevador ou foguete.
-			</p>
-
-			<h5>Dolly</h5>
-
-			<p>
-				Parecido com o movimento de panorâmica vertical, o dolly é o deslocamento vertical da câmera, 
-				executado por equipamentos especiais como tripés hidráulicos de estúdio ou gruas.
-				Os movimentos de dolly não servem para deixar evidente a altura de algo, mas são muito bons
-				para revelar detalhes de um element vertical. Um bom exemplo seria o dolly de uma exuberante 
-				atriz e seu maravilhoso vestido. A proximidade do movimento deixaria em evidência as
-				curvas da atriz e os detalhes de seu vestido, ao passo que o deslocamento lento com o campo
-				visual fechado revelaria um ar de suspense muito excitante.
-			</p>
-
-			<h5>Zoom</h5>
-
-			<p>
-				Os últimos movimentos não são tecnicamente movimentos, pois são feitos por um jogo de
-				lentes e não pelo movimento da câmera. O zoom demorou a aparecer, pois foi preciso antes
-				desenvolver o jogo de lentes compostas. Se repararmos na parte da frente de uma câmera de
-				cinema, veremos um tubo com uma lente em uma das extremidades. Este tubo é chamado
-				de objetiva.
-			</p>
-
-			<p>
-				Hoje a objetiva é algo comum, em câmeras domésticas as objetivas já são tão pequenas que se
-				escondem dentro do corpo da câmera, mas nem sempre foi assim. No começo da história do
-				cinema, as câmeras não possuíam o jogo de lentes objetivas e a distância tinha de ser previamente 
-				definida para se determinar qual lente focal seria utilizada. Por isso, uma das grandes
-				características dos primeiros filmes foi a utilização do plano geral, grosseiramente utilizado
-				para tudo. A propósito, foi por causa da revolução das lentes que se pôde desenvolver a 
-				linguagem de planos e movimentos.
-			</p>
-
-			<p>
-				Na medida em que evoluía a tecnologia das lentes, tornou-se possível captar imagens nos
-				mais variados ângulos e rapidamente os diretores descobriram que era possível aproximar ou
-				afastar a imagem pelo uso do jogo de lentes no momento em que gravavam. A estes efeitos
-				foram dados os nomes de zoom-in e zoom-out.
-			</p>
-			
-			<h5>Zoom–in</h5>
-
-			<p>
-				É a aproximação da imagem pelo jogo de lentes. Devido à facilidade de execução, este movimento é 
-				muito utilizado tanto em publicidade quanto em telejornalismo. Com a capacidade
-				de ir de um plano aberto para um plano mais fechado, o zoom-in tem a capacidade de direcionar 
-				o olhar do espectador para uma característica específica da imagem, como por exemplo,
-				uma pessoa em meio a uma multidão ou uma palavra no meio de um texto.
-			</p>
-
-			<h5>Zoom–out</h5>
-
-			<p>
-				O afastamento da imagem pelo jogo de lentes da câmera. Tendo o resultado oposto do zoom-in, 
-				o movimento de afastamento da imagem é muito eficiente para revelar, no tempo necessário, 
-				o ambiente ao redor do seu objeto de gravação.
-			</p>
-
-			<h5>Câmeras com atitude</h5>
-
-			<p>
-				Além dos planos e movimentos, a câmera ainda pode transmitir ideias através de sua atitude
-				em cena, recebendo assim alguns nomes característicos como, câmera baixa, câmera alta,
-				câmera subjetiva e câmera nervosa.
-			</p>
-
-			<h5>Câmera baixa/câmera nervosa</h5>
-
-			<p>
-				A altura em que posicionamos a câmera no momento da filmagem influencia muito a visão do
-				espectador - uma câmera posicionada acima da cabeça do personagem passa a nítida sensação 
-				de inferioridade, enquanto a câmera baixa passa a sensação de superioridade. Em campanhas 
-				eleitorais é muito comum a utilização da camera baixa para a produção da imagem de
-				superioridade do candidato. A altura da câmera também pode ser utilizada em um filme para
-				desenhar as linhas de força de cada personagem.
-			</p>
-
-			<h5>Câmera subjetiva</h5>
-
-			<p>
-				Em cena, a câmera pode tomar duas atitudes, a do espectador assistindo o desenrolar do fato
-				de forma neutra e passiva (camera objetiva), ou a atitude do personagem, interagindo na cena
-				ao similar sua visão (câmera subjetiva). O recurso da câmera subjetiva é amplamente utilizado
-				na construção narrativa de muitos filmes.
-			</p>
-
-			<h5>Câmera nervosa</h5>
-
-			<p>
-				Cineastas do passado em constante busca de novas formas de expressar sentimentos pelas
-				telas dos cinemas passaram a buscar atitudes alternativas para o uso da câmera. Uma delas
-				foi tentar passar o desconforto e a incerteza da seqüência através do balanço desconcertante
-				da câmera no momento de gravação.
-			</p>
-
 
 			<h5 class="barlow center-align">A T I V I D A D E S</h5>
-            <p style="margin-top: 16px"><b>1.</b> Separar trechos de um plano sequencia fornecido pelo professor, apontando quais 
-				planos de enquadramento e movimentos de camera se tratam.</p>
-			<p style="margin-top: 16px"><b>2.</b> Aplicar conceitos sobre planos de enquadramento e movimentos de camera para novas 
-				capas de video.</p>
-			<p style="margin-top:16px"><b>3.</b> Realizar edições em um novo projeto.</p>
-
+            <p style="margin-top: 16px"><b>1.</b> Escolher um video e realizar cortes, escolhendo 5 ou 10 cenas, usando as ferramentas
+				de edição.</p>
+			<p style="margin-top: 16px"><b>2.</b> Crar um projeto TOP 10 ou TOP 5 com as melhores cenas do video.</p>
 
 		</div>
     </div>
