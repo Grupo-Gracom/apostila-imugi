4d9d72bd57ff3d03cd7fd7920eafb297d5df6ee3
yurifa <yurialcantara03@gmail.com>
2020-09-01 10:48:05 -0300

    finalizando unidade 7

--- resources/views/apostila6/unidade7/animacao_basica.blade.php
+++ resources/views/apostila6/unidade7/animacao_basica.blade.php
@@ -41,8 +41,8 @@
 			<h5>Propriedades das Layers</h5>
 
 			<p>
-				Cada layer possui propriedades que podem ser modifi cadas e animadas. O grupo básico de
-				propriedades editáveis chama-se transform (que inclui propriedades como posição e opacidade).
+				Cada layer possui propriedades que podem ser modificadas e animadas. O grupo básico de
+				propriedades editáveis chama-se transform (que incluipropriedades como posição e opacidade).
 			</p>
 
 			<p>
@@ -66,10 +66,81 @@
 				É possível expandir o contorno da layer para exibir as propriedades da layer e alterar os valores
 				das propriedades. Note que a maioria das propriedades possui um cronômetro. Qualquer pro-
 				priedade que tenha um símbolo cronômetro pode ser animada. Vale lembrar que, se várias
-				layers esti verem selecionadas e você alterar uma propriedade para uma layer, a propriedade
+				layers estiverem selecionadas e você alterar uma propriedade para uma layer, a propriedade
 				será alterada para todas as layers selecionadas.
 			</p>
-	
+
+			<h5>Animation, keyframes e expressions</h5>
+
+			<p>
+				Se pensarmos em animação, percebemos que basicamente animar é mudança e essa mudança 
+				ocorre com o passar do tempo. Assim, o usuário anima uma layer ou um efeito em uma
+				layer fazendo com que uma ou mais de suas propriedades mude com o tempo.
+			</p>
+
+			<div class="container">
+				<figure>
+					<img src="{{ asset('assets/img/video_edition/unit7/imagem_unit7_6.png') }}" alt="guia1">
+				</figure>
+			</div>
+
+			<p>
+				A. Cronômetro ativo B. Cronômetro inativo
+			</p>
+
+			<p>
+				É possível animar as propriedades da layer usando keyframe, expressions ou ambos. Muitas
+				predefinições de animação incluem keyframes e expressions para que o usuário possa simplesmente 
+				aplicar a animação predefinida para a layer para alcançar um resultado animado
+				complexo. O after effects trabalha com keyframes e expressions em dois modos básicos: modo
+				barra de layer (modo padrão, que mostra as layers como barras de duração, com keyframes e
+				expressions alinhadas verticalmente com suas propriedades na timeline) ou modo editor de
+				gráfico (mostra keyframes e resultados de expressions em gráficos de velocidade).
+			</p>
+
+			<h5>Keyframe</h5>
+
+			<p>
+				Os keyframes são usados para definir parâmetros para movimento, efeitos, áudio e muitas outras 
+				propriedades, alterando-os ao longo do tempo. Um keyframe marca um ponto no tempo
+				onde o usuário especifica um valor para uma ou mais propriedades da layer. Geralmente, ao 
+				usar keyframes para criar uma alteração ao longo do tempo, usa-se pelo menos dois keyframes 
+				(um para o estado do início da alteração e um para o novo estado no final da alteração).
+				Os valores entre os keyframes são interpolados.
+			</p>
+
+			<p>
+				Vale lembrar que quando o símbolo do cronômetro está inativo para uma propriedade, a
+				propriedade não possui keyframes. Ao alterar o valor de propriedade da layer enquanto o cronômetro 
+				estiver inativo, esse valor permanece o mesmo durante a duração da layer. Quando
+				o cronômetro está ativo para uma propriedade específica, o after irá definir automaticamente
+				ou alterar um keyframe para a propriedade no momento atual sempre que o usuário altera o
+				valor da propriedade.
+			</p>
+
+			<p>
+				Ao desativar o cronômetro , todos os keyframes para essa propriedade da layer são excluídos
+				e o valor constante para essa propriedade se torna o valor no momento atual.
+			</p>
+
+			<h5>Precompose</h5>
+
+			<p>
+				Para agrupar layers que já estão em uma composição é preciso pré-compor essas layers. O
+				precompose coloca essas layers em uma nova composição, que substituias layers na composição 
+				original. A nova composição pré-composta torna-se a fonte de uma única layer na
+				composição original e aparece no painel projeto, ficando disponível para renderização ou uso
+				em qualquer outra composição. É possível pré-compor composições adicionando uma composição 
+				existente a outra. Pré-compor uma única layer é útil para adicionar propriedades de
+				transformação a uma layer e influenciar a ordem em que os elementos de uma composição
+				são renderizados.
+			
+			</p>
+
+			<h5 class="barlow center-align">A T I V I D A D E S</h5>
+            <p style="margin-top: 16px"><b>1.</b> Crie um efeito de chuva que reaja na cena, fazendo com que a mesma ao cair no chão
+			produza respingos e quando colidir com objetos da cena possa criar sub partículas.</p>
+
 		</div>
     </div>
 
