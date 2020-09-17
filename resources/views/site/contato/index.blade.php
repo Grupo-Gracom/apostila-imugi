17fed463bb7189aa2870ce8c0637cb77c6fae4e5
Will Rodrigues <wrodrigues153@gmail.com>
2020-09-16 21:13:29 -0300

    nova unidade inserida em contato

--- resources/views/site/contato/index.blade.php
+++ resources/views/site/contato/index.blade.php
@@ -11,7 +11,7 @@
                                         <span >NOSSAS </span> <br>
                                         <div style="height:30px;"></div>
                                         <span >UNIDADES </span>
-                                        
+
                                 </h5>
                                 <div style="margin-top:120px;"></div>
                             </div>
@@ -22,7 +22,7 @@
         <!-- escolas carousel style 1 -->
         <section style="padding-bottom:0px!important;">
             <div class="container">
-                <div class="row">    
+                <div class="row">
                     <div class="swiper-slider-clients swiper-container">
                         <div class="swiper-wrapper">
                             <div class="swiper-slide text-center">
@@ -33,7 +33,7 @@
                             <div class="swiper-slide text-center">
                                 <a data-fancybox="gallery" href="{{asset('assets/site/images/unidades/02.jpg')}}" data-caption="Conheça a Imugi - Tecnológia Educacional">
                                     <img src="{{asset('assets/site/images/unidades/02.jpg')}}" alt="" style="width:80%!important;">
-                                </a>    
+                                </a>
                             </div>
                             <div class="swiper-slide text-center">
                                 <a data-fancybox="gallery" href="{{asset('assets/site/images/unidades/03.jpg')}}" data-caption="Conheça a Imugi - Tecnológia Educacional">
@@ -43,32 +43,32 @@
                             <div class="swiper-slide text-center">
                                 <a data-fancybox="gallery" href="{{asset('assets/site/images/unidades/04.jpg')}}" data-caption="Conheça a Imugi - Tecnológia Educacional">
                                     <img src="{{asset('assets/site/images/unidades/04.jpg')}}" alt="" style="width:80%!important;">
-                                </a>    
+                                </a>
                             </div>
                             <div class="swiper-slide text-center">
                                 <a data-fancybox="gallery" href="{{asset('assets/site/images/unidades/05.jpg')}}" data-caption="Conheça a Imugi - Tecnológia Educacional">
                                     <img src="{{asset('assets/site/images/unidades/05.jpg')}}" alt="" style="width:80%!important;">
-                                </a>    
+                                </a>
                             </div>
                             <div class="swiper-slide text-center">
                                 <a data-fancybox="gallery" href="{{asset('assets/site/images/unidades/06.jpg')}}" data-caption="Conheça a Imugi - Tecnológia Educacional">
                                     <img src="{{asset('assets/site/images/unidades/06.jpg')}}" alt="" style="width:80%!important;">
-                                </a>    
+                                </a>
                             </div>
                             <div class="swiper-slide text-center">
-                                <a data-fancybox="gallery" href="{{asset('assets/site/images/unidades/07.1.jpg')}}" data-caption="Conheça a Imugi - Tecnológia Educacional">    
+                                <a data-fancybox="gallery" href="{{asset('assets/site/images/unidades/07.1.jpg')}}" data-caption="Conheça a Imugi - Tecnológia Educacional">
                                     <img src="{{asset('assets/site/images/unidades/07.jpg')}}" alt="" style="width:80%!important;">
-                                </a>    
+                                </a>
                             </div>
                             <div class="swiper-slide text-center">
                                 <a data-fancybox="gallery" href="{{asset('assets/site/images/unidades/08.1.jpg')}}" data-caption="Conheça a Imugi - Tecnológia Educacional">
                                     <img src="{{asset('assets/site/images/unidades/08.jpg')}}" alt="" style="width:80%!important;">
                                 </a>
                             </div>
-                        
+
                         </div>
                     </div>
-                </div>    
+                </div>
             </div>
         </section>
         <!-- client carousel style 1 end-->
@@ -77,7 +77,7 @@
                 <div class="row">
                     <div class="col-12 text-center wow fadeIn">
                         <h5 class="text-uppercase depoimentos">
-                            ESTUDE NA IMUGI MAIS <br> 
+                            ESTUDE NA IMUGI MAIS <br>
                             <div style="height:10px;"></div>
                             PRÓXIMA DE VOCÊ
                         </h5>
@@ -137,7 +137,7 @@
                         <div class="d-inline-block margin-20px-bottom"><i class="icon-map text-white-2 icon-round-small bg-deep-green"></i></div>
                             <div class="alt-font text-extra-dark-gray font-weight-600 margin-10px-bottom">MANAUS / AM</div>
                             <p class="width-75 mx-auto">
-                                Av. Getúlio Vargas, 204 - Centro 
+                                Av. Getúlio Vargas, 204 - Centro
                             </p>
                         </div>
                     </div>
@@ -155,13 +155,25 @@
                     <!-- end features box item -->
                 </div>
                 <div class="row mt-4">
-                     <!-- end features box item -->
-                     <div class="col-12 col-lg-4 col-md-6 wow fadeInUp last-paragraph-no-margin" data-wow-delay="0.6s">
+                     <!-- start features box item -->
+                     <div class="col-12 col-lg-4 col-md-6 md-margin-four-bottom sm-margin-30px-bottom wow fadeInUp last-paragraph-no-margin" data-wow-delay="0.2s">
                         <div class="bg-white border-color-extra-medium-gray border text-center padding-eighteen-tb border-radius-4 position-relative h-100">
-                            <div class="d-inline-block margin-20px-bottom"><i class="icon-map text-white-2 icon-round-small bg-dark-gray"></i></div>
-                            <div class="alt-font text-extra-dark-gray font-weight-600 margin-10px-bottom">ANAPÓLIS / GO</div>
+                            <div class="d-inline-block margin-20px-bottom"><i class="icon-map text-white-2 icon-round-small bg-deep-green"></i></div>
+                            <div class="alt-font text-extra-dark-gray font-weight-600 margin-10px-bottom">ANÁPOLIS / GO</div>
                             <p class="width-75 mx-auto">
-                                em implantação
+                            Rua Barão de Cotegipe, 333 / Centro <br>
+                            (62) 3702-7950
+                            </p>
+                        </div>
+                    </div>
+                    <!-- end features box item -->
+                     <!-- start features box item -->
+                     <div class="col-12 col-lg-4 col-md-6 md-margin-four-bottom sm-margin-30px-bottom wow fadeInUp last-paragraph-no-margin" data-wow-delay="0.2s">
+                        <div class="bg-white border-color-extra-medium-gray border text-center padding-eighteen-tb border-radius-4 position-relative h-100">
+                            <div class="d-inline-block margin-20px-bottom"><i class="icon-map text-white-2 icon-round-small bg-deep-green"></i></div>
+                            <div class="alt-font text-extra-dark-gray font-weight-600 margin-10px-bottom">SOROCABA / SP</div>
+                            <p class="width-75 mx-auto">
+                            Rua Padre Luiz 387 / Centro
                             </p>
                         </div>
                     </div>
@@ -188,7 +200,7 @@
                     <div class="col-12 col-lg-7 text-center margin-100px-bottom sm-margin-40px-bottom">
                         <div class="position-relative overflow-hidden w-100">
                             <span class="text-small text-outside-line-full alt-font font-weight-600 text-uppercase">Alguma dúvida? Deixe sua mensagem. <br>
-                            
+
                         </span>
                         </div>
                     </div>
@@ -214,16 +226,16 @@
                 {{ csrf_field() }}
                     <div class="row">
                         <div class="col-12 col-lg-6">
-                            <input type="text" class="bg-transparent border-color-medium-dark-gray medium-input" name="nome" placeholder="Nome"> 
+                            <input type="text" class="bg-transparent border-color-medium-dark-gray medium-input" name="nome" placeholder="Nome">
                         </div>
                         <div class="col-12 col-lg-6">
-                            <input type="text" class="telefone bg-transparent border-color-medium-dark-gray medium-input" name="telefone" placeholder="Telefone"> 
-                        </div>
-                        <div class="col-12 col-lg-6">  
-                            <input type="text" class="bg-transparent border-color-medium-dark-gray medium-input" name="estado" placeholder="Estado"> 
+                            <input type="text" class="telefone bg-transparent border-color-medium-dark-gray medium-input" name="telefone" placeholder="Telefone">
                         </div>
                         <div class="col-12 col-lg-6">
-                            <input type="text" class="bg-transparent border-color-medium-dark-gray medium-input" name="cidade" placeholder="Cidade"> 
+                            <input type="text" class="bg-transparent border-color-medium-dark-gray medium-input" name="estado" placeholder="Estado">
+                        </div>
+                        <div class="col-12 col-lg-6">
+                            <input type="text" class="bg-transparent border-color-medium-dark-gray medium-input" name="cidade" placeholder="Cidade">
                         </div>
                         <div class="col-12">
                             <textarea class="bg-transparent border-color-medium-dark-gray medium-textarea" name="mensagem" rows="6" placeholder="escreva sobre sua dúvida"></textarea>
@@ -248,7 +260,7 @@
                             QUALQUER DÚVIDA
                         </h5>
                     </div>
-                    <div class="col-md-4">    
+                    <div class="col-md-4">
                         <a href="https://api.whatsapp.com/send?1=pt_BR&phone=5519994567998" target="_blank" class="btn btn-large btn btn-success  btn-rounded mr-0">FALE CONOSCO</a>
                     </div>
                 </div>
