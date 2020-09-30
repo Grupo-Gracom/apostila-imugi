53346e2891349a226337e25eef91dbcc907cac9d
Will Rodrigues <wrodrigues153@gmail.com>
2020-09-30 19:31:57 -0300

    nova unidade

--- resources/views/site/contato/index.blade.php
+++ resources/views/site/contato/index.blade.php
@@ -201,6 +201,17 @@
                             </p>
                         </div>
                     </div>
+
+                     <!-- start features box item -->
+                     <div class="col-12 col-lg-4 col-md-6 md-margin-four-bottom sm-margin-30px-bottom wow fadeInUp last-paragraph-no-margin" data-wow-delay="0.2s">
+                        <div class="bg-white border-color-extra-medium-gray border text-center padding-eighteen-tb border-radius-4 position-relative h-100">
+                            <div class="d-inline-block margin-20px-bottom"><i class="icon-map text-white-2 icon-round-small bg-dark-gray"></i></div>
+                            <div class="alt-font text-extra-dark-gray font-weight-600 margin-10px-bottom">Belém / PA</div>
+                            <p class="width-75 mx-auto">
+                                Travessa Padre Eutiquio, 981 - Batista Campos 
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
